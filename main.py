8baa59cb98de0a4c195616cc5db1b4f129ebb31b
Placinta <placinta@176134.no-reply.drupal.org>
2012-11-17 22:18:07 +0200

    Change some size constants.

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ class MainWindow(QtGui.QMainWindow):
             self.jQuery = ''
 
         QtNetwork.QNetworkProxyFactory.setUseSystemConfiguration(True)
+        self.setGeometry(QtCore.QRect(200, 50, 1024, 600))
 
         # Graphics view
         self.view = QtGui.QGraphicsView()
@@ -70,9 +71,13 @@ class MainWindow(QtGui.QMainWindow):
         self.view.setOptimizationFlags(QtGui.QGraphicsView.DontSavePainterState)
         self.view.setFrameShape(QtGui.QFrame.NoFrame)
         self.view.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-        self.view.resize(1024, 768)
+        self.view.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.view.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.view.resize(800, 600)
 
-        self.view.setSceneRect(QtCore.QRectF(0, 0, 640, 480))
+        self.scene_width = 1024
+        self.scene_height = 600
+        self.view.setSceneRect(QtCore.QRectF(0, 0, self.scene_width, self.scene_height))
         #self.view.set
 
         self.view.setRenderHints(QtGui.QPainter.Antialiasing | QtGui.QPainter.SmoothPixmapTransform)
@@ -181,7 +186,7 @@ class MainWindow(QtGui.QMainWindow):
         self.fast_view.setFocus()
 
         position_start = self.view.mapToGlobal(self.view.pos())
-        position_end = position_start + QtCore.QPoint(600, 400)
+        position_end = position_start + QtCore.QPoint(self.scene_width, self.scene_height - 100)
         self.geometry = (position_start.x(), position_start.y(), position_end.x(), position_end.y())
         print self.geometry
 
@@ -266,7 +271,7 @@ class MainWindow(QtGui.QMainWindow):
         cv.SetData(cv_im, image.tostring())
 
         fourcc = cv.CV_FOURCC('D','I','V','X')
-        fps = 15
+        fps = 25
         width, height = cv.GetSize(cv_im)
         #print width, height
         self.writer = cv.CreateVideoWriter('out3.avi', int(fourcc), fps, (int(width), int(height)), 1)
@@ -280,7 +285,7 @@ class MainWindow(QtGui.QMainWindow):
         self.frames_count = 1
 
         timer = QtCore.QTimer()
-        time_interval = 1000 / 15
+        time_interval = 1000 / 25
         timer.setInterval(time_interval)
         timer.timeout.connect(self.grabFrame)
         timer.start()
